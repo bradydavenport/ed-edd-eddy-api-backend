5e6c92090cc70192da589850423ac8f9ed521436
Brady Davenport <brady.davenport@pm.me>
2022-08-30 10:16:34 -0600

    error handling routes

---
+++ src/server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const notFound = require('./error-handlers/404');
+const errorHandler = require('./error-handlers/500');
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+app.use(express.json);
+
+app.get('/', (req, res) => {
+  res.send('Thanks for visiting the \"Ed, Edd, n Eddy\" API! The site is under construction')
+});
+app.get('*', notFound);
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  start: () => app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+};
