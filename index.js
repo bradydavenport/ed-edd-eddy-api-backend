c920f34e79617b412b2969d1eeced65d94e5da5a
Brady Davenport <brady.davenport@pm.me>
2022-09-07 13:45:45 -0600

    refactor

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const { sequelize } = require('./src/models');
 const server = require('./src/server');
 
 sequelize.sync()
-.then(() => {
-  console.log('Successful Connection')
-})
+.then(() => console.log('Successful Connection'))
 .catch(err => console.error(err));
 
 server.start();
