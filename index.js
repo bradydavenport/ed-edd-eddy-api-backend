92401acb63730aa2cf6f88a8111751049a7131ca
Brady Davenport <brady.davenport@pm.me>
2022-09-16 12:02:39 -0600

    connection message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { sequelize } = require('./src/models');
 const server = require('./src/server');
 
 sequelize.sync()
-  .then(() => console.log('Successful Connection'))
+  .then(() => console.log('Successful connection to database'))
   .catch(err => console.error(err));
 
 server.start();
