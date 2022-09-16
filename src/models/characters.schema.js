3000596567dd02d973f16887c46c47ca24f381af
Brady Davenport <brady.davenport@pm.me>
2022-09-16 12:08:16 -0600

    adds more fields

--- src/models/characters.schema.js
+++ src/models/characters.schema.js
@@ -1,10 +1,41 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('characters', {
+  return sequelize.define('character', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    nicknames: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      default: [],
+      allowNull: true,
+    },
+    credits: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      default: [],
+      allowNull: true,
+    },
+    portrayer: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    hairColor: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    friends: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      default: [],
+      allowNull: true,
+    },
+    enemies: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
+    },
+    firstAppearance: {
+      type: DataTypes.STRING,
+      allowNull: true,
     },
   });
 };
