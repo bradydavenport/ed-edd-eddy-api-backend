62f63f80ccb86b084db476b43bb17a08fb53c923
Brady Davenport <brady.davenport@pm.me>
2022-10-20 12:33:59 -0600

    changing postgres db connection

--- src/models/index.js
+++ src/models/index.js
@@ -2,24 +2,27 @@
 
 const { Sequelize, DataTypes } = require('sequelize');
 require('dotenv').config();
+const modelInterface = require('./modelInterface');
 const characterSchema = require('./characters.schema');
 
 const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite::memory'
   : process.env.DATABASE_URL || 'postgres://localhost:5432/ed-edd-eddy-api';
 
-const sequelize = new Sequelize(DATABASE_URL); /*{
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-}); */
+const sequelize = new Sequelize(DATABASE_URL);
+
+// const sequelize = new Sequelize(DATABASE_URL, {
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false,
+//     },
+//   },
+// });
 
 const CharacterModel = characterSchema(sequelize, DataTypes);
 
 module.exports = {
   sequelize,
-  CharacterModel,
+  characterInterface: new modelInterface(CharacterModel),
 };
