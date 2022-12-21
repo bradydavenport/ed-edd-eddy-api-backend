eae29864eb6ec7c956d321d9684acc8197f7b2d6
Brady Davenport <brady.davenport@pm.me>
2022-12-21 13:49:46 -0700

    dialect options

--- src/models/index.js
+++ src/models/index.js
@@ -9,16 +9,14 @@ const DATABASE_URL = process.env.NODE_ENV === 'test'
   ? 'sqlite::memory'
   : process.env.DATABASE_URL || 'postgres://localhost:5432/ed-edd-eddy-api';
 
-const sequelize = new Sequelize(DATABASE_URL);
-
-// const sequelize = new Sequelize(DATABASE_URL, {
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false,
-//     },
-//   },
-// });
+const sequelize = new Sequelize(DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 const CharacterModel = characterSchema(sequelize, DataTypes);
 
