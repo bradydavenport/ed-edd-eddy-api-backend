5d545db9c3a7984fe89b929dafd777bd61cadd9e
Brady Davenport <brady.davenport@pm.me>
2023-01-05 19:06:57 -0700

    adds episode schema

---
+++ src/models/episodes.schema.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('episodes', {
+    season: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    season_episode_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    series_episode_number: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    title_card_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    episode_title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    scams_that_take_place: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: true,
+    },
+    title_reference: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    episode_synopsis: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    episode_airdate: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  { timestamps: false });
+};
